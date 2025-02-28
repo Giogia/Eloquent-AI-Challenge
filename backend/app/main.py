b2baa78abc6b8720f0a4510fb66b6f307d91b0c7
Private <private@Mac.station>
2025-02-28 21:45:50 +0100

    Added Database Connection and Base Model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+
+from app.db.database import engine
+from app.db.models import Base
 from app.routers import healthcheck
 
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(healthcheck.router)
