b5c7a2c7bc1ea53aebc435fb22b8f0b08c3e46ce
Private <private@Mac.station>
2025-03-01 00:17:53 +0100

    Added Cors Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from app.db.database import engine
@@ -10,6 +11,19 @@ load_dotenv()
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(healthcheck.router)
 app.include_router(chat.router)
